9ff6a2f30ec1689f491a50869db2acd4632b50d6
Steeven sayago <stevenandre0407@gmail.com>
2024-11-28 13:44:09 -0500

    Terminando avances en el proyecto de la api de perros

--- perros.js
+++ perros.js
@@ -1,6 +1,14 @@
+const api = axios.create({
+    baseURL: 'https://api.thedogapi.com/v1/',   
+});
+api.defaults.headers.common['X-API-KEY'] = 'live_gNxya64DQIkhHCbhRdgnsUCXFozMh7vBWb693HCsS6ccEnD7C5tmxd4o5zkFCWSu'
+
+
 const key = "live_gNxya64DQIkhHCbhRdgnsUCXFozMh7vBWb693HCsS6ccEnD7C5tmxd4o5zkFCWSu"
 const url = `https://api.thedogapi.com/v1/images/search?limit=6&api_key=${key}`
-const urlFavoritos= `https://api.thedogapi.com/v1/favourites?&api_key=${key}`
+const urlFavoritos= `https://api.thedogapi.com/v1/favourites?`
+const urlSubirImagen = `https://api.thedogapi.com/v1/images/upload`  
+
 const btn = document.getElementById("cambiarImagen")
 
 const spanError = document.getElementById("error")
@@ -37,9 +45,14 @@ const cargarPerrosAleatorios = async () => {
 const cargarPerrosFavoritos = async () => {
     const contenedorFavoritos = document.getElementById("favoritos")
     try {
-        const response = await fetch(urlFavoritos)
+        const response = await fetch(urlFavoritos,{
+            method: "GET",
+            headers: {
+                'X-API-KEY': `${key}`
+            }
+        })
         const data = await response.json()
-        console.log(data);
+        //console.log(data);
         
         if(response.status!==200){
             spanError.innerHTML ="Hubo un Error en la solicitud de los favoritos " + response.status
@@ -80,23 +93,12 @@ const cargarPerrosFavoritos = async () => {
 }
 
 const subirPerrosFavoritos = async (id) => {
-        const response = await fetch(urlFavoritos, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body:JSON.stringify({
-                image_id: id
-            }),
-        })
-        
-        const data = await response.json()
-        
-        console.log(response);
-
-        if(response.status!==200){
-            spanError.innerHTML="hubo un error:" + response.status + data.message;
-            console.log(response.status + data.message)
+    const {data, status} = await api.post('favourites?',{
+        image_id: id
+    })
+        if(status!==200){
+            spanError.innerHTML="hubo un error:" + status + data.message;
+            console.log(        status + data.message)
         }else{
             console.log("Se guardo en favoritos");
             cargarPerrosFavoritos()
@@ -109,6 +111,9 @@ const eliminarFavorito = async (id)=>{
 
     const response = await fetch(urlEliminar, {
         method: 'DELETE',
+        headers: {
+            'X-API-KEY': `${key}`
+        }
     })
     const data = await response.json();
 
@@ -122,6 +127,45 @@ const eliminarFavorito = async (id)=>{
     }
 
 }
+
+const subirImagenPerro = async()=>{
+    const formulario = document.getElementById("formulario")
+    const formData = new FormData(formulario)
+
+    //console.log(formData.get('file'))
+
+
+    const response = await fetch(urlSubirImagen, { 
+        method: 'POST',
+        headers: {
+            'X-API-KEY': `${key}`
+        },
+        body: formData,
+    })
+    const data = await response.json();
+    
+    if(data.status!==200){
+        spanError.innerHTML="hubo un error:" + response.status + data.message;
+        console.log(response.status + data.message)
+        //console.log({data})
+    }else{
+        console.log("Se agrego la imagen");
+    }
+
+    subirPerrosFavoritos(data.id)   
+    document.getElementById('preview').src = ""
+}
+
+document.getElementById('file').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('preview').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
 
 cargarPerrosAleatorios()
 cargarPerrosFavoritos()
